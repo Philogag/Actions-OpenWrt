bf099332853b1f4b5ee44717d948c84fb5853cde
philogag <philogag@qq.com>
2025-07-13 22:22:40 +0800

    fix bad custom feed path

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo "src-link custom $PWD" >> feeds.conf.default
+echo "src-link custom $PWD/../custom" >> feeds.conf.default
