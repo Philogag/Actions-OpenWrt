cdac1a62538f3ae75d367be11417f9a02189ab37
philogag <philogag@qq.com>
2025-07-13 22:28:59 +0800

    fix diy 1

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo "src-link custom $PWD/../custom" >> feeds.conf.default
+#echo "src-link custom $PWD/../custom" >> feeds.conf.default
+cp -r ../custom/* ./package/
